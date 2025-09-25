f893e8ab5c603318acf6ca4412985a4ff51f3802
aryopratamaa <aryo.pratama@uinsu.ac.id>
2025-09-26 06:15:17 +0700

    05.15

--- app/src/main/java/com/aryo/mandarinapp/DewasaOnsiteBeginner.kt
+++ app/src/main/java/com/aryo/mandarinapp/DewasaOnsiteBeginner.kt
@@ -1,5 +1,8 @@
 package com.aryo.mandarinapp
 
+import android.content.Intent
+import android.widget.Button
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +14,25 @@ class DewasaOnsiteBeginner : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_dewasa_onsite_beginner)
+
+        val btnPilihKelasA = findViewById<Button>(R.id.btnKelasA)
+        val noHp = "6282161965317"
+        val teks = "Halo, saya ingin memilih kelas beginner A untuk kelas onsite Mandarin App"
+        btnPilihKelasA.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://wa.me/$noHp?text=$teks")
+            startActivity(intent)
+        }
+
+        val btnPilihKelasB = findViewById<Button>(R.id.btnKelasB)
+        val noHp2 = "6282161965317"
+        val teks2 = "Halo, saya ingin memilih kelas beginner B untuk kelas onsite Mandarin App"
+        btnPilihKelasB.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://wa.me/$noHp2?text=$teks2")
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
