d02c8b8c8f7cfd8fcd58a3b2a7581b87fb6b0148
aryopratamaa <aryo.pratama@uinsu.ac.id>
2025-09-25 22:48:37 +0700

    22.48

--- app/src/main/java/com/aryo/mandarinapp/Mentor.kt
+++ app/src/main/java/com/aryo/mandarinapp/Mentor.kt
@@ -1,6 +1,8 @@
 package com.aryo.mandarinapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,29 @@ class Mentor : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_mentor)
+
+        val btnWhatsapp = findViewById<Button>(R.id.ic_whatsappAryo)
+        btnWhatsapp.setOnClickListener {
+            Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnInstagram = findViewById<Button>(R.id.ic_instagramAryo)
+        btnInstagram.setOnClickListener {
+            Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnFb = findViewById<Button>(R.id.ic_fbAryo)
+        btnFb.setOnClickListener {
+            Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnYoutube = findViewById<Button>(R.id.ic_youtubeAryo)
+        btnYoutube.setOnClickListener {
+            Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show()
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
