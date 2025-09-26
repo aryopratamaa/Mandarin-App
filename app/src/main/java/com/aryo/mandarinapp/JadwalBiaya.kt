5ca9454adbe14f883ea66ca6b729a840f4663e95
aryopratamaa <aryo.pratama@uinsu.ac.id>
2025-09-26 23:28:28 +0700

    23.28

---
+++ app/src/main/java/com/aryo/mandarinapp/JadwalBiaya.kt
@@ -0,0 +1,20 @@
+package com.aryo.mandarinapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class JadwalBiaya : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_jadwal_biaya)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/aryo/mandarinapp/MainActivity.kt
+++ app/src/main/java/com/aryo/mandarinapp/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.Button
 import android.content.Intent
+import android.net.Uri
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +20,12 @@ class MainActivity : AppCompatActivity() {
         val btnAnak = findViewById<Button>(R.id.btnAnak)
         val btnPerpus = findViewById<Button>(R.id.btnPerpus)
         val btnJadwal = findViewById<Button>(R.id.btnJadwal)
+        val btnSemuaMentor = findViewById<Button>(R.id.btnSemuaMentor)
+        val btnDaring = findViewById<Button>(R.id.btnDaring)
+        val btnPrivate = findViewById<Button>(R.id.btnPrivate)
+        val btnTelpon = findViewById<Button>(R.id.btnTelpon)
+
+
 
         btnDewasa.setOnClickListener {
             val intent = Intent(this, dewasa::class.java)
@@ -26,20 +33,36 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnAnak.setOnClickListener {
-            Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Masih masa pengembangan", Toast.LENGTH_SHORT).show()
+        }
+
+        btnPrivate.setOnClickListener {
+            Toast.makeText(this, "Masih masa pengembangan", Toast.LENGTH_SHORT).show()
         }
 
         btnPerpus.setOnClickListener {
-            Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Perpustakaan::class.java)
+            startActivity(intent)
         }
 
         btnJadwal.setOnClickListener {
-            Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, JadwalBiaya::class.java)
+            startActivity(intent)
         }
 
-        val btnSemuaMentor = findViewById<Button>(R.id.btnSemuaMentor)
         btnSemuaMentor.setOnClickListener {
             val intent = Intent(this, Mentor::class.java)
+            startActivity(intent)
+        }
+
+        btnDaring.setOnClickListener {
+            val intent = Intent(this, DetailKelasOnline::class.java)
+            startActivity(intent)
+        }
+
+        btnTelpon.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:085158335498")
             startActivity(intent)
         }
 
